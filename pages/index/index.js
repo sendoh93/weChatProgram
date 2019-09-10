3de74387b128d17e26177428c25df9f57667220e
ypf <hzypf_11@163.com>
2019-09-11 02:01:42 +0800

    联调好商圈和品类接口，完善了申请入驻界面

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,25 +10,12 @@ Page({
     townName: '万达广场',
     category: '品类',
     categoryId: 0,
+    addressId:0,
     addressName: '湖滨银泰',
-    adrressArrayList: [{
-      id: 0,
-      name: '万达广场'
-    }, {
-      id: 1,
-      name: '城西银泰'
-    }, {
-      id: 2,
-      name: '湖滨银泰'
-    }, {
-      id: 3,
-      name: '中大银泰'
-    }, {
-      id: 4,
-      name: '水晶城'
-    }, ],
-    adrressArray: ['万达广场', '城西银泰', '湖滨银泰', '中大银泰', '水晶城'],
-    categoryArray: ['烧烤', '奶茶', '中餐', '西餐', '炸鸡', '港式', '西北菜'],
+    addressIndex:0,
+    categoryIndex:0,
+    adrressArrayList: [],
+    categoryArrayList:[],
     goodList: [{
       "name": "小老头烧烤",
       "price": "180元/位",
@@ -75,19 +62,38 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-      // app.ajax({
-      //   url: '/merchant-business',
-      //   method: "GET",
-      //   data: {
-      //     fields: "name",
-      //   },
-      //   success: function (res) {
-      //     console.log(res)
-      //     that.setData({
-
-      //     })
-      //   }
-      // })
+      app.ajax({
+        url: '/merchant-business',
+        method: "GET",
+        data: {
+          fields: "id,name",
+        },
+        success: function (res) {
+          let adrressArrayList = res.data.rspdata || []
+          that.setData({
+            adrressArrayList
+          })
+        },
+        fail:function(res){
+          console.log(res)
+        }
+      })
+      app.ajax({
+        url: '/merchant-category',
+        method: "GET",
+        data: {
+          fields: "id,name",
+        },
+        success: function (res) {
+          let categoryArrayList = res.data.rspdata || []
+          that.setData({
+            categoryArrayList
+          })
+        },
+        fail: function (res) {
+          console.log(res)
+        }
+      })
     } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
@@ -127,20 +133,50 @@ Page({
     })
   },
   choosAdress(e) {
-    console.log(e)
+    console.log(parseInt(this.data.adrressArrayList[parseInt(e.detail.value)].id))
     let id = e.currentTarget.dataset.id
     switch (id) {
       case 1:
         this.setData({
-          addressName: this.data.adrressArray[e.detail.value],
+          addressName: this.data.adrressArrayList[parseInt(e.detail.value)].name,
           //e.detail.value 代表item的position。
-          addressId: parseInt(e.detail.value)
+          addressId: parseInt(this.data.adrressArrayList[parseInt(e.detail.value)].id),
+          addressIndex: e.detail.value,  //每次选择了下拉列表的内容同时修改下标然后修改显示的内容，显示的内容和选择的内容一致
+        })
+        app.ajax({
+          url: '/merchant-business/0',
+          method: "GET",
+          data: {
+          },
+          success: function (res) {
+            console.log(res)
+          },
+          fail: function (res) {
+            console.log(res)
+          }
         })
         break;
       case 2:
         this.setData ({
-          category: this.data.categoryArray[e.detail.value],
-          categoryId: parseInt(e.detail.value)
+          category: this.data.categoryArrayList[e.detail.value].name,
+          categoryId: parseInt(this.data.categoryArrayList[parseInt(e.detail.value)].id),
+          categoryIndex:e.detail.value,
+        })
+        app.ajax({
+          url: '/merchant-business',
+          method: "GET",
+          data: {
+            fields: "id,name",
+          },
+          success: function (res) {
+            let adrressArrayList = res.data.rspdata || []
+            that.setData({
+              adrressArrayList
+            })
+          },
+          fail: function (res) {
+            console.log(res)
+          }
         })
         break
     }
